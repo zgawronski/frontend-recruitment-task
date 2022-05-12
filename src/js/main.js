7fd58b08c0aad8ee3f0dbb8a81819d1666abb4b5
Zbigniew Gawroński <zbigniewgawro@gmail.com>
2022-05-12 19:23:04 +0200

    px to em

--- src/js/main.js
+++ src/js/main.js
@@ -1,12 +1,14 @@
 const alertButton = document.querySelector('#alertButton');
 const alertPopup = document.querySelector('#alertPopup');
 
+// function to open popup
 alertButton.addEventListener('click', () => {
   if (typeof alertPopup.showModal === 'function') {
     alertPopup.showModal();
   }
 });
 
+// function to close popup
 alertPopup.addEventListener('click', (event) => {
   const rect = alertPopup.getBoundingClientRect();
   const isInPopup =
