a3e7bea539c503380be1d417761baa2bba193b36
Zbigniew Gawroński <zbigniewgawro@gmail.com>
2022-05-12 19:18:37 +0200

    arrow functions

--- src/js/main.js
+++ src/js/main.js
@@ -1,13 +1,13 @@
 const alertButton = document.querySelector('#alertButton');
 const alertPopup = document.querySelector('#alertPopup');
 
-alertButton.addEventListener('click', function onOpen() {
+alertButton.addEventListener('click', () => {
   if (typeof alertPopup.showModal === 'function') {
     alertPopup.showModal();
   }
 });
 
-alertPopup.addEventListener('click', function (event) {
+alertPopup.addEventListener('click', (event) => {
   const rect = alertPopup.getBoundingClientRect();
   const isInPopup =
     rect.top <= event.clientY && event.clientY <= rect.top + rect.height && rect.left <= event.clientX && event.clientX <= rect.left + rect.width;
