f1b585e7ce2b29be44f2b261cf6708cdd4e4a6d4
Zbigniew Gawroński <zbigniewgawro@gmail.com>
2022-05-12 15:18:27 +0200

    gulp fix

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,7 @@
 const { src, dest, watch, series, parallel } = require('gulp');
 
 const sourcemaps = require('gulp-sourcemaps');
-const sass = require('gulp-sass');
+const sass = require('gulp-sass')(require('sass'));
 const concat = require('gulp-concat');
 const uglify = require('gulp-uglify');
 const postcss = require('gulp-postcss');
@@ -10,45 +10,33 @@ const cssnano = require('cssnano');
 var replace = require('gulp-replace');
 
 // File paths
-const files = { 
-    scssPath: 'src/scss/**/*.scss',
-    jsPath: 'src/js/**/*.js'
+const files = {
+  scssPath: 'src/scss/**/*.scss',
+  jsPath: 'src/js/**/*.js',
 };
 
-function scssTask(){    
-    return src(files.scssPath)
-        .pipe(sourcemaps.init()) // initialize sourcemaps first
-        .pipe(sass([])) // compile SCSS to CSS
-        .pipe(postcss([ autoprefixer(), cssnano() ])) // PostCSS plugins
-        .pipe(sourcemaps.write('.')) // write sourcemaps file in current directory
-        .pipe(dest('dist')
-    ); // put final CSS in dist folder
+function scssTask() {
+  return src(files.scssPath)
+    .pipe(sourcemaps.init()) // initialize sourcemaps first
+    .pipe(sass([])) // compile SCSS to CSS
+    .pipe(postcss([autoprefixer(), cssnano()])) // PostCSS plugins
+    .pipe(sourcemaps.write('.')) // write sourcemaps file in current directory
+    .pipe(dest('dist')); // put final CSS in dist folder
 }
 
-function jsTask(){
-    return src([
-        files.jsPath
-        ])
-        .pipe(concat('all.js'))
-        .pipe(uglify())
-        .pipe(dest('dist')
-    );
+function jsTask() {
+  return src([files.jsPath]).pipe(concat('all.js')).pipe(uglify()).pipe(dest('dist'));
 }
 
 var cbString = new Date().getTime();
-function cacheBustTask(){
-    return src(['index.html'])
-        .pipe(replace(/cb=\d+/g, 'cb=' + cbString))
-        .pipe(dest('.'));
+function cacheBustTask() {
+  return src(['index.html'])
+    .pipe(replace(/cb=\d+/g, 'cb=' + cbString))
+    .pipe(dest('.'));
 }
 
-function watchTask(){
-    watch([files.scssPath, files.jsPath], 
-        parallel(scssTask, jsTask));    
+function watchTask() {
+  watch([files.scssPath, files.jsPath], parallel(scssTask, jsTask));
 }
 
-exports.default = series(
-    parallel(scssTask, jsTask), 
-    cacheBustTask,
-    watchTask
-);
+exports.default = series(parallel(scssTask, jsTask), cacheBustTask, watchTask);
